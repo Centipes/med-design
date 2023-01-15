29bbb46f6bbc6dfa0c7da1ea5c2ed4131aadf7ed
Arina Pankova <arinpank@gmail.com>
2023-01-16 00:19:51 +0300

    fix arrows position v4.1

--- js/scripts.js
+++ js/scripts.js
@@ -26,8 +26,8 @@ window.addEventListener('DOMContentLoaded', event => {
     // $('#arrow-header').css("top", (50-13*window.devicePixelRatio) + "%");
     // $('#arrow-header').css("", (1.1*window.devicePixelRatio) + "em");
     $('#arrow-header').css("transform", "translate(0, " + -(2*window.devicePixelRatio) + "px)");
-    $('#arrow-map').css("transform", "translate(0, " + -(4*window.devicePixelRatio) + "px)");
-    $('.arrow-service').css('transform', 'translate(0,' + -(3*window.devicePixelRatio) + 'px)');
+    $('#arrow-map').css("transform", "translate(0, " + -(5*window.devicePixelRatio/1.25) + "px)");
+    $('.arrow-service').css('transform', 'translate(0,' + -(4*window.devicePixelRatio/1.25) + 'px)');
     
 
     let disp = setInterval(() => {
