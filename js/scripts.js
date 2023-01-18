48e69098dbb8a84a94b3ad40c2b5bc63d2452094
Arina Pankova <arinpank@gmail.com>
2023-01-18 23:00:37 +0300

    fix arrows position v5.0

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // $('#arrow-header').css("transform", "translate(0, " + (-window.devicePixelRatio) + "%)");
     // $('#arrow-header').css("top", (50-13*window.devicePixelRatio) + "%");
     // $('#arrow-header').css("", (1.1*window.devicePixelRatio) + "em");
-    $('#arrow-header').css("transform", "translate(0, " + -(2*window.devicePixelRatio) + "px)");
+    // $('#arrow-header').css("transform", "translate(0, " + -(2*window.devicePixelRatio) + "px)");
     $('#arrow-map').css("transform", "translate(0, " + -(3*window.devicePixelRatio/1.25+1.25/window.devicePixelRatio*2.5/window.devicePixelRatio+0.1*(1.25-window.devicePixelRatio)) + "px)");
     $('.arrow-service').css('transform', 'translate(0,' + -(3*window.devicePixelRatio/1.25+1.25/window.devicePixelRatio*0.8/window.devicePixelRatio) + 'px)');
     console.log(-(3*window.devicePixelRatio/1.25+1.25/window.devicePixelRatio*2.5/window.devicePixelRatio));
